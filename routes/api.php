c4074179d21688106f3d93c6aec28aaae2e72e7d
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-01-03 00:10:53 -0300

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('/series', \App\Http\Controllers\Api\SeriesController::class);
+Route::get('/series/{series}/seasons', function (\App\Models\Series $series) {
+    return $series->seasons;
+});
+
+Route::get('/series/{series}/episodes', function (\App\Models\Series $series) {
+    return $series->episodes;
+});
+
+Route::patch('/episodes/{episode}', function (\App\Models\Episode $episode, Request $request) {
+    $episode->watched = $request->watched;
+    $episode->save();
+
+    return $episode;
+});
